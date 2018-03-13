a7464579d374c536478e420e07b34564dfd80050
jingkeling <841127052@qq.com>
2018-03-14 00:34:26 +0800

    Initial commit

---
+++ src/components/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Row>
+      <i-Col span="12">col-12</i-Col>
+      <i-Col span="12">col-12</i-Col>
+    </Row>
+    <br>
+    <Row>
+      <i-Col span="8">col-8</i-Col>
+      <i-Col span="8">col-8</i-Col>
+      <i-Col span="8">col-8</i-Col>
+    </Row>
+    <br>
+    <Row>
+      <i-Col span="6">col-6</i-Col>
+      <i-Col span="6">col-6</i-Col>
+      <i-Col span="6">col-6</i-Col>
+      <i-Col span="6">col-6</i-Col>
+    </Row>
+  </div>
+</template>
+<script>
+  export default {
+
+  }
+</script>
+
